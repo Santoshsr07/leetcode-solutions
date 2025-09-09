b36096340b84fb5b78f4e19261b3971554398107
Santoshsr07 <santoshsr7096@gmail.com>
2025-09-09 22:34:33 +0530

    Solving LeetCode Questions

---
+++ Leetcode/Linkedlist/Add_Two_Numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+struct ListNode
+{
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
+{
+}
+int main()
+{
+    printf("L1");
+    while (!NULL)
+    {
+        struct ListNode *l1 =(struct ListNode*)malloc(sizeof(struct ListNode));
+        scanf("%d",&l1->val);
+        l1->next=l1->next+1;
+    }
+    
+    
+    
+
+    struct ListNode *l2 =(struct ListNode*)malloc(sizeof(struct ListNode));
+
+    return 0;
+}
