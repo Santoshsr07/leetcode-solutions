0ace90fea1310a70d1ac316151c56cdc2f91c1d9
Santoshsr07 <santoshsr7096@gmail.com>
2026-08-19 20:48:56 +0530

    101
    Symmetric Tree

--- Tree/101. Symmetric Tree.java
+++ Tree/101. Symmetric Tree.java
@@ -1,46 +1,47 @@
-// 101. Symmetric Tree
-// Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+/* 101. Symmetric Tree
 
- 
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 
-// Example 1:
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: true
 
+Example 2:
+Input: root = [1,2,2,null,3,null,3]
+Output: false
 
-// Input: root = [1,2,2,3,4,4,3]
-// Output: true
-// Example 2:
+Constraints:
+The number of nodes in the tree is in the range [1, 1000].
+-100 <= Node.val <= 100
 
+Follow up: Could you solve it both recursively and iteratively?
 
-// Input: root = [1,2,2,null,3,null,3]
-// Output: false
- 
+https://leetcode.com/problems/symmetric-tree/description/?envType=study-plan-v2&envId=top-100-liked
 
-// Constraints:
+*/
+import java.util.LinkedList;
 
-// The number of nodes in the tree is in the range [1, 1000].
-// -100 <= Node.val <= 100
- 
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
-// Follow up: Could you solve it both recursively and iteratively?
-
-// ******************code******************
-
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- * }
- */
 
 class Solution {
 
     public boolean isSymmetric(TreeNode root) {
-        return mirror(root.left, root.right);
+        return helper(root.left, root.right);
     }
 
-    public boolean mirror(TreeNode left, TreeNode right) {
+    public boolean helper(TreeNode left, TreeNode right) {
 
         if (left == null && right == null)
             return true;
@@ -51,7 +52,6 @@ class Solution {
         if (left.val != right.val)
             return false;
 
-        return mirror(left.left, right.right) &&
-                mirror(left.right, right.left);
+        return helper(left.left, right.right) && helper(left.right, right.left);
     }
 }
