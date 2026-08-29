a3c7aa9cbbb5408a9343d34535ca7b99e10ec6fe
Santoshsr07 <santoshsr7096@gmail.com>
2026-08-29 22:47:33 +0530

    16
    3Sum Closest

---
+++ Two Pointers/16. 3Sum Closest.java
@@ -0,0 +1,60 @@
+/* 16. 3Sum Closest
+
+Given an integer array nums of length n and an integer target, find three integers at distinct indices in nums such that the sum is closest to target.
+
+Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.
+
+Example 1:
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+
+Example 2:
+Input: nums = [0,0,0], target = 1
+Output: 0
+Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+
+Constraints:
+3 <= nums.length <= 500
+-1000 <= nums[i] <= 1000
+-104 <= target <= 104
+
+https://leetcode.com/problems/3sum-closest/description/ */
+
+import java.util.Arrays;
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int ans = Integer.MAX_VALUE;
+        int close = 0;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == target)
+                    return sum;
+
+                int rem = Math.abs(target - sum);
+
+                if (rem < ans) {
+                    ans = rem;
+                    close = sum;
+                }
+
+                if (sum < target)
+                    left++;
+                else
+                    right--;
+            }
+        }
+
+        return close;
+    }
+}
