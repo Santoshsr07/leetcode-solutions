c1f1c2c510d1695ddd77292194a27fbdaedb59dd
Santoshsr07 <santoshsr7096@gmail.com>
2026-09-21 23:35:05 +0530

    124
    Binary Tree Maximum Path Sum

--- Tree/124. Binary Tree Maximum Path Sum.java
+++ Tree/124. Binary Tree Maximum Path Sum.java
@@ -1,43 +1,42 @@
-// 124. Binary Tree Maximum Path Sum
-// A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+/* 124. Binary Tree Maximum Path Sum
 
-// The path sum of a path is the sum of the node's values in the path.
+A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
 
-// Given the root of a binary tree, return the maximum path sum of any non-empty path.
+The path sum of a path is the sum of the node's values in the path.
 
-// Example 1:
+Given the root of a binary tree, return the maximum path sum of any non-empty path.
 
-// Input: root = [1,2,3]
-// Output: 6
-// Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
-// Example 2:
+Example 1:
+Input: root = [1,2,3]
+Output: 6
+Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
 
-// Input: root = [-10,9,20,null,null,15,7]
-// Output: 42
-// Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
+Example 2:
+Input: root = [-10,9,20,null,null,15,7]
+Output: 42
+Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
 
-// Constraints:
+Constraints:
+The number of nodes in the tree is in the range [1, 3 * 104].
+-1000 <= Node.val <= 1000
 
-// The number of nodes in the tree is in the range [1, 3 * 104].
-// -1000 <= Node.val <= 1000
-// ------------code -----------------
+https://leetcode.com/problems/binary-tree-maximum-path-sum/description/?envType=study-plan-v2&envId=top-100-liked */
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
- * }
- */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 class Solution {
+
     int result = Integer.MIN_VALUE;
 
     public int maxPathSum(TreeNode root) {
